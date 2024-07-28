66d9d5816786e2711f7140651ab334ff23fbd623
kristianmandrup <kmandrup@gmail.com>
2024-07-28 21:33:22 +0700

    Add shadcn foundations

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,6 +1,7 @@
 <nav class="bg-gray-100 py-4">
 	<ul class="flex items-center gap-4 px-4 text-sm">
 		<li><a href="/">Home</a></li>
+		<li><a href="/authentication" data-sveltekit-preload-data>Auth</a></li>
 		<li><a href="/projects" data-sveltekit-preload-data>Projects</a></li>
 		<li><a href="/members" data-sveltekit-preload-data>Members</a></li>
 		<li><a href="/board-v1">Board v1</a></li>
